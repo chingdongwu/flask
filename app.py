fbc5c0d1df42e7d22523021e38344f1a2e80da3f
dd <remix@gmail.com>
2022-09-28 19:13:10 +0800

    bulma css完成

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 
-from flask import Flask, render_template, request, session
-from pm25 import get_pm25
+from flask import Flask, render_template, request, session, abort
+from pm25 import get_pm25, get_six_pm25, get_countys, get_county_pm25
 import json
 from stock import get_stocks
 
 app = Flask(__name__)
 
 app.secret_key = "python is good"
+sort = False
 
 
 @app.route('/<name>')
@@ -57,36 +58,54 @@ def get_stock():
     return render_template('./stock.html', date=get_date(), stocks=get_stocks())
 
 
-sort = False
-
-
-@app.route('/pm25', methods=['POST', 'GET'])
+@app.route('/pm25', methods=['GET', 'POST'])
 def pm25():
     global sort
     if request.method == 'POST':
         sort = not sort
+
     date = get_date()
     columns, values = get_pm25(sort)
-
-    # print(columns, values)
-    return render_template('pm25.html', date=date, columns=columns, values=values)
+    return render_template('./pm25.html', **locals())
 
 
 @app.route('/pm25-charts')
 def pm25_charts():
 
-    return render_template('pm25_charts.html')
+    return render_template('./pm25-charts-bulma.html', countys=get_countys())
 
 
-@app.route('/pm25-json')
+@app.route('/pm25-json', methods=['POST'])
 def pm25_json():
     columns, values = get_pm25()
     site = [value[1] for value in values]
     pm25 = [value[2] for value in values]
+    date = values[0][-1]
+
+    return json.dumps({'date': date, 'site': site, 'pm25': pm25}, ensure_ascii=False)
+
+
+@app.route('/pm25-six-json', methods=['POST'])
+def pm25_six_json():
+    values = get_six_pm25()
+    site = [value[0] for value in values]
+    pm25 = [value[1] for value in values]
 
     return json.dumps({'site': site, 'pm25': pm25}, ensure_ascii=False)
 
 
-if __name__ == '__main__':
+@app.route('/pm25-county/<county>', methods=['POST'])
+def pm25_county_json(county):
+    try:
+        values = get_county_pm25(county)
+        site = [value[0] for value in values]
+        pm25 = [value[1] for value in values]
 
+        return json.dumps({'site': site, 'pm25': pm25}, ensure_ascii=False)
+    except:
+        return '取得資料失敗!'
+
+
+if __name__ == '__main__':
+    pm25_json()
     app.run(debug=True)
